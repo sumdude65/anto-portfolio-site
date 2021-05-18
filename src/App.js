87c861d0b1a747a28dd1bcec78b43b5a4336ad65
sumdude65 <imadeosaretin@gmail.com>
2021-05-18 23:41:09 +0100

    featured post component
    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Home } from "./components/pages/homepage";
+import "./style.css";
+
+export const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
---
+++ src/components/pages/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FeaturedPost } from "../featuredPost";
+
+import "../style.css";
+
+export const Home = () => {
+  return (
+    <div className="homepage">
+      <FeaturedPost />
+    </div>
+  );
+};
