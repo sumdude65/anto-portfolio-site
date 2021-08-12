dd1406dec00098ac49eb006ff103058caad450f2
sumdude65 <imadeosaretin@gmail.com>
2021-08-13 04:09:55 +0800

    complete just navbar tweak needed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Navbar = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggle = () => {
+    const firstLine = document.getElementById("navbar-1");
+    const secondLine = document.getElementById("navbar-2");
+    const thirdLine = document.getElementById("navbar-3");
+    if (secondLine.style.transform === "rotate(45deg)") {
+      firstLine.style.display = "inline-block";
+      secondLine.style.transform = "rotate(0deg)";
+      thirdLine.style.transform = "rotate(0deg)";
+      setShowModal(false);
+    } else {
+      firstLine.style.display = "none";
+      secondLine.style.transform = "rotate(45deg)";
+      thirdLine.style.transform = "rotate(-45deg)";
+      setShowModal(true);
+    }
+  };
   return (
     <header>
       <div className="logo">
@@ -11,6 +29,29 @@ export const Navbar = () => {
         <a href="/">About</a>
         <a href="/">Contact</a>
       </div>
+      <div className="navbar-toggle" onClick={handleToggle}>
+        <span id="navbar-1"></span>
+        <span id="navbar-2"></span>
+        <span id="navbar-3"></span>
+      </div>
+      {/*navigation modal for phones*/}
+      {showModal ? (
+        <div className="navbar-modal">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
     </header>
   );
 };
